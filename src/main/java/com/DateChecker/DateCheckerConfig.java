a2632b6249b297b2a2cc21894383ef3bcb8e9916
utharba <msimon363@gmail.com>
2026-08-29 13:00:15 -0600

    Test of example without changes

--- src/main/java/com/DateChecker/DateCheckerConfig.java
+++ src/main/java/com/DateChecker/DateCheckerConfig.java
@@ -1,11 +1,11 @@
-package com.example;
+package com.DateChecker;
 
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
 
 @ConfigGroup("example")
-public interface ExampleConfig extends Config
+public interface DateCheckerConfig extends Config
 {
 	@ConfigItem(
 		keyName = "greeting",
